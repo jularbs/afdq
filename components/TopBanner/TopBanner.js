b45a153e443c5d0aaee69f6bf40f42a8350be38c
ralph jularbal <ralphjularbal@yahoo.com>
2022-10-06 15:51:09 +0800

    sponsor tab

--- components/TopBanner/TopBanner.js
+++ components/TopBanner/TopBanner.js
@@ -4,16 +4,13 @@ const TopBanner = () => {
   return (
     <div className={styles["afdq-topbar-container"]}>
       <div className={styles["announcement-container"]}>
-        {/* <div className={styles["image-placeholder"]}>
-          <img src="/afdq/model2022.png" alt="" height="100%" width="auto" />
-        </div> */}
         <div className={styles["text-container"]}>
           <div className={styles["header"]}>
             Vote for the Netizen's Choice Award
           </div>
           <div className={styles["sub-header"]}>
             Voting is from October 8 to 29, 2022
-          </div>  
+          </div>
         </div>
         <div className={styles["sponsor-container"]}>
           <img
