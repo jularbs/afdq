28c8b626f0f4f299aaac7ad82872cfe35464d759
ralph jularbal <ralphjularbal@yahoo.com>
2022-10-06 15:40:11 +0800

    use 1line sponsor image

--- components/SponsorsTab/SponsorsTabTest.js
+++ components/SponsorsTab/SponsorsTabTest.js
@@ -4,26 +4,7 @@ const SponsorsTabTest = () => {
   return (
     <div className={styles["afdq-sponsors-tab-container"]}>
       <div className={styles["header"]}>in partnership with</div>
-      <div className={styles["sponsor-logo-container"]}>
-        <div className={styles["item"]}>
-          <img src="/afdq/sponsor/unique-ad.png" width="100%" alt="" />
-        </div>
-        <div className={styles["item"]}>
-          <img src="/afdq/sponsor/whiterose-ad.png" width="100%" alt="" />
-        </div>
-        <div className={styles["item"]}>
-          <img src="/afdq/sponsor/charm-ad.png" width="100%" alt="" />
-        </div>
-        <div className={`${styles["item"]} ${styles["hz"]}`}>
-          <img src="/afdq/sponsor/birch-ad.png" width="100%" alt="" />
-        </div>
-        <div className={`${styles["item"]} ${styles["hz"]}`}>
-          <img src="/afdq/sponsor/tanduay-ad.png" width="100%" alt="" />
-        </div>
-        <div className={`${styles["item"]} ${styles["hz"]}`}>
-          <img src="/afdq/sponsor/palmo-ad.jpg" width="100%" alt="" />
-        </div>
-      </div>
+      <img src="/afdq/sponsors1line.png" className={styles.sponsorWrapper} />
     </div>
   );
 };
